cf2b765b0db3742bd8ad99daa5ab3587b08e5c73
nithinbairu <nithinbairu08@gmail.com>
2025-11-01 16:13:30 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+  const [data] = useState({ temperature: 25, conditions: "Sunny" });
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay 
+        temperature={data.temperature} 
+        conditions={data.conditions} 
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
